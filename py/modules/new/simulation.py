a68fb5004503f1d969f92424dce187b54d729aeb
rodandres <andreswii6@hotmail.com>
2026-07-21 11:47:14 -0500

    New classes to support translational sims

---
+++ py/modules/new/simulation.py
@@ -0,0 +1,150 @@
+from dataclasses import dataclass, field
+from pathlib import Path
+import sys
+
+REPO_ROOT = Path(__file__).resolve().parents[2]
+if str(REPO_ROOT) not in sys.path:
+    sys.path.insert(0, str(REPO_ROOT))
+
+import numpy as np
+
+from py.modules.new.spacecraft import Spacecraft
+
+@dataclass
+class SimulationData:
+    max_sim_time: float
+
+    dt_master: float
+    dt_propagation: float
+    
+    spacecrafts: list
+
+    t = 0.0
+    tick = 0
+    verbose: bool = False
+
+class Simulation:
+    def __init__(self, max_sim_time: float, dt_propagation: float, verbose: bool = False):        
+
+        simulation_data = SimulationData(
+            max_sim_time=max_sim_time,
+            dt_propagation=dt_propagation,
+            verbose=verbose
+        )
+
+        self.simulation_data = simulation_data
+
+        
+    def add_spacecraft(self,
+                       spacecraft_mass: float,
+                       spacecraft_inertia_tensor: np.ndarray,
+                       spacecraft_actuators: list, # NOTE: Add the type of the list
+                       spacecraft_sensors: list, # NOTE: Add the type of the list
+                       spacecraft_controllers: list, # NOTE: Add the type of the list
+                       spacecraft_guidance: list, # NOTE: Add the type of the list
+                       spacecraft_dt_nav: float,
+                       spacecraft_dt_guid: float,
+                       spacecraft_dt_control: float,
+                       initial_state: np.ndarray, # pos, vel, quat, omega in inertial frame (numpy array of shape (13,))
+                       name: str |None=None,
+                       verbose=False):
+        
+
+        # Check mass type
+        if not isinstance(spacecraft_mass, (int, float)):
+            raise TypeError("Spacecraft mass must be a number (int or float).")
+        
+        # Check inertia tensor type and shape
+        if not isinstance(spacecraft_inertia_tensor, np.ndarray):
+            raise TypeError("Spacecraft inertia tensor must be a numpy ndarray.")
+
+        if spacecraft_inertia_tensor.shape != (3, 3):
+            raise ValueError("Spacecraft inertia tensor must be a 3x3 matrix.")
+
+        # Check actuators, sensors, controllers, and guidance types
+        # TODO: Implement type checks for actuators, sensors, controllers, and guidance if specific types are defined.
+
+        # Check initial state type and shape
+        if not isinstance(initial_state, np.ndarray):
+            raise TypeError("Initial state must be a numpy ndarray.")
+        if initial_state.shape != (13,):
+            raise ValueError("Initial state must be a numpy array of shape (13,) representing pos, vel, quat, omega in inertial frame.")
+
+
+        if name is None:
+            name = f"Spacecraft_{len(self.simulation_data.spacecrafts)+1}"
+
+        spacecraft = Spacecraft(
+            name=name,
+            mass=spacecraft_mass,
+            inertia_tensor=spacecraft_inertia_tensor,
+            actuators=spacecraft_actuators,
+            sensors=spacecraft_sensors,
+            controllers=spacecraft_controllers,
+            guidance=spacecraft_guidance,
+            dt_nav=spacecraft_dt_nav,
+            dt_guid=spacecraft_dt_guid,
+            dt_control=spacecraft_dt_control,
+            initial_state=initial_state,
+            verbose=verbose
+        )
+
+        self.simulation_data.spacecrafts[name] = {
+            "spacecraft": spacecraft
+        }
+
+        if self.simulation_data.verbose:
+            print(f"Spacecraft '{name}' added to the simulation.")
+
+    def __set_dt_master(self):
+        dts = [self.simulation_data.dt_propagation]  # Start with the master propagation time step
+        for spacecraft in self.simulation_data.spacecrafts.values():
+            dt_nav, dt_guid, dt_control = spacecraft.get_gnc_dts()
+            dts.append((dt_nav, dt_guid, dt_control))
+
+        self.simulation_data.dt_master = min(dts)
+
+    def __set_solver(self):
+        # Placeholder for solver selection logic
+        # This method can be expanded to select and configure the appropriate numerical solver based on simulation settings.
+        pass
+        
+    def __init_simulation(self):
+        if self.simulation_data.verbose:
+            print("Initializing simulation...")
+        
+
+        self.__set_dt_master()
+        self.__set_solver()
+    
+
+        if self.simulation_data.verbose:
+            print("Simulation initialized.")
+
+    def __propagate_dynamics(self):
+        # NOTE: This method should implement the dynamics propagation logic for the simulation.
+        pass
+
+    def simulate(self):
+        self.__init_simulation()
+
+        if self.simulation_data.verbose:
+            print("Starting simulation...")
+
+        while self.simulation_data.t < self.simulation_data.max_sim_time:
+            
+            self.simulation_data.t = self.simulation_data.tick * self.simulation_data.dt_master
+
+            for spacecraft in self.simulation_data.spacecrafts:
+                spacecraft.compute_tick_step(self.simulation_data.tick, self.simulation_data.dt_master)
+
+            if self.simulation_data.tick % self.simulation_data.dt_propagation == 0:
+                self.__propagate_dynamics()
+
+            self.simulation_data.tick += 1
+
+        # TODO: Add logging and history recording here if needed.
+        
+
+        if self.simulation_data.verbose:
+            print("Simulation completed.")
---
+++ py/modules/new/spacecraft.py
@@ -0,0 +1,248 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass, field
+
+import numpy as np
+
+@dataclass
+class SpacecraftData:
+    name: str
+
+    mass: float
+    inertia_tensor: np.ndarray
+
+    actuators: list # NOTE: Add the type of the list
+    sensors: list # NOTE: Add the type of the list
+    controllers: list # NOTE: Add the type of the list
+    guidance: list # NOTE: Add the type of the list
+
+    dt_nav: float 
+    dt_guid: float
+    dt_control: float
+    verbose: bool
+
+    # Spacraft state variables in inertial frame (same as the inertial frame of the simulation)
+    true_pos: np.ndarray = field(default_factory=lambda: np.zeros(3))
+    true_vel: np.ndarray = field(default_factory=lambda: np.zeros(3))
+    true_accel: np.ndarray = field(default_factory=lambda: np.zeros(3))
+
+    true_q: np.ndarray = field(default_factory=lambda: np.array([1, 0, 0, 0]))
+    true_omega: np.ndarray = field(default_factory=lambda: np.zeros(3))
+    true_alpha: np.ndarray = field(default_factory=lambda: np.zeros(3))
+
+    # Spacecraft state variables in sensor frame (same as the sensor frame of the spacecraft)
+    measured_pos: np.ndarray = field(default_factory=lambda: np.zeros(3))
+    measured_vel: np.ndarray = field(default_factory=lambda: np.zeros(3))
+    measured_accel: np.ndarray = field(default_factory=lambda: np.zeros(3))
+
+    measured_q: np.ndarray = field(default_factory=lambda: np.array([1, 0, 0, 0]))
+    measured_omega: np.ndarray = field(default_factory=lambda: np.zeros(3))
+    measured_alpha: np.ndarray = field(default_factory=lambda: np.zeros(3))
+
+    # Spacraft state variables in body frame (same as the body frame of the spacecraft)
+    body_pos: np.ndarray = field(default_factory=lambda: np.zeros(3))
+    body_vel: np.ndarray = field(default_factory=lambda: np.zeros(3))
+    body_accel: np.ndarray = field(default_factory=lambda: np.zeros(3))
+
+    body_q: np.ndarray = field(default_factory=lambda: np.array([1, 0, 0, 0]))
+    body_omega: np.ndarray = field(default_factory=lambda: np.zeros(3))
+    body_alpha: np.ndarray = field(default_factory=lambda: np.zeros(3))
+
+    # Reference state variables in inertial frame (same as the inertial frame of the simulation)
+    true_ref_pos: np.ndarray = field(default_factory=lambda: np.zeros(3))
+    true_ref_vel: np.ndarray = field(default_factory=lambda: np.zeros(3))
+    true_ref_accel: np.ndarray = field(default_factory=lambda: np.zeros(3))
+
+    true_ref_q: np.ndarray = field(default_factory=lambda: np.array([1, 0, 0, 0]))
+    true_ref_omega: np.ndarray = field(default_factory=lambda: np.zeros(3))
+    true_ref_alpha: np.ndarray = field(default_factory=lambda: np.zeros(3))
+
+    # Reference state variables in sensor frame (same as the sensor frame of the spacecraft)
+    measured_ref_pos: np.ndarray = field(default_factory=lambda: np.zeros(3))
+    measured_ref_vel: np.ndarray = field(default_factory=lambda: np.zeros(3))
+    measured_ref_accel: np.ndarray = field(default_factory=lambda: np.zeros(3))
+    measured_ref_q: np.ndarray = field(default_factory=lambda: np.array([1, 0, 0, 0]))
+    measured_ref_omega: np.ndarray = field(default_factory=lambda: np.zeros(3))
+    measured_ref_alpha: np.ndarray = field(default_factory=lambda: np.zeros(3))
+
+
+class Spacecraft():
+
+    def __init__(self, name: str, mass: float, inertia_tensor: np.ndarray, 
+                 actuators: list, sensors: list, controllers: list, guidance: list,
+                 dt_nav: float, dt_guid: float, dt_control: float, initial_state: np.ndarray, verbose: bool = False):
+
+        spacecraft_data = SpacecraftData(
+            name=name,
+
+            mass=mass,
+            inertia_tensor=inertia_tensor,
+
+            actuators=actuators,
+            sensors=sensors,
+            controllers=controllers,
+            guidance=guidance,
+
+            dt_nav=dt_nav,
+            dt_guid=dt_guid,
+            dt_control=dt_control,
+
+            verbose=verbose
+        )
+
+        self.spacecraft_data = spacecraft_data
+
+        # Initialize the spacecraft state variables based on the provided initial state
+        self.spacecraft_data.true_pos = initial_state[0:3]
+        self.spacecraft_data.true_vel = initial_state[3:6]
+        self.spacecraft_data.true_q = initial_state[6:10]
+        self.spacecraft_data.true_omega = initial_state[10:13]
+
+        if self.spacecraft_data.verbose:
+            print(f"="*10 + " Spacecraft created successfully. " + "="*10)
+            self.show_spacecraft_basic_info()
+            self.show_spacecraft_gnc_info()
+            self.show_spacecraft_state_info()
+            print("="*10 + " End of spacecraft information. " + "="*10)            
+
+    def show_spacecraft_basic_info(self):
+        print(f"Spacecraft Name: {self.spacecraft_data.name}")
+        print(f"Mass: {self.spacecraft_data.mass} kg")
+        print(f"Inertia Tensor: \n{self.spacecraft_data.inertia_tensor}")
+
+    def show_spacecraft_gnc_info(self):
+        print(f"Actuators: {self.spacecraft_data.actuators}")
+        print(f"Sensors: {self.spacecraft_data.sensors}")
+        print(f"Controllers: {self.spacecraft_data.controllers}")
+        print(f"Guidance: {self.spacecraft_data.guidance}")
+
+        print(f"Navigation Time Step: {self.spacecraft_data.dt_nav} s")
+        print(f"Guidance Time Step: {self.spacecraft_data.dt_guid} s")
+        print(f"Control Time Step: {self.spacecraft_data.dt_control} s")
+
+    def show_spacecraft_state_info(self):
+        print("True State (Inertial Frame):")
+        print(f"Position: {self.spacecraft_data.true_pos}")
+        print(f"Velocity: {self.spacecraft_data.true_vel}")
+        print(f"Acceleration: {self.spacecraft_data.true_accel}")
+        print(f"Quaternion: {self.spacecraft_data.true_q}")
+        print(f"Angular Velocity: {self.spacecraft_data.true_omega}")
+        print(f"Angular Acceleration: {self.spacecraft_data.true_alpha}")
+
+        print("\nMeasured State (Sensor Frame):")
+        print(f"Position: {self.spacecraft_data.measured_pos}")
+        print(f"Velocity: {self.spacecraft_data.measured_vel}")
+        print(f"Acceleration: {self.spacecraft_data.measured_accel}")
+        print(f"Quaternion: {self.spacecraft_data.measured_q}")
+        print(f"Angular Velocity: {self.spacecraft_data.measured_omega}")
+        print(f"Angular Acceleration: {self.spacecraft_data.measured_alpha}")
+
+        print("\nBody State (Body Frame):")
+        print(f"Position: {self.spacecraft_data.body_pos}")
+        print(f"Velocity: {self.spacecraft_data.body_vel}")
+        print(f"Acceleration: {self.spacecraft_data.body_accel}")
+        print(f"Quaternion: {self.spacecraft_data.body_q}")
+        print(f"Angular Velocity: {self.spacecraft_data.body_omega}")
+        print(f"Angular Acceleration: {self.spacecraft_data.body_alpha}")
+
+    def show_spacecraft_reference_info(self):
+        print("True Reference State (Inertial Frame):")
+        print(f"Position: {self.spacecraft_data.true_ref_pos}")
+        print(f"Velocity: {self.spacecraft_data.true_ref_vel}")
+        print(f"Acceleration: {self.spacecraft_data.true_ref_accel}")
+        print(f"Quaternion: {self.spacecraft_data.true_ref_q}")
+        print(f"Angular Velocity: {self.spacecraft_data.true_ref_omega}")
+        print(f"Angular Acceleration: {self.spacecraft_data.true_ref_alpha}")
+
+        print("\nMeasured Reference State (Sensor Frame):")
+        print(f"Position: {self.spacecraft_data.measured_ref_pos}")
+        print(f"Velocity: {self.spacecraft_data.measured_ref_vel}")
+        print(f"Acceleration: {self.spacecraft_data.measured_ref_accel}")
+        print(f"Quaternion: {self.spacecraft_data.measured_ref_q}")
+        print(f"Angular Velocity: {self.spacecraft_data.measured_ref_omega}")
+        print(f"Angular Acceleration: {self.spacecraft_data.measured_ref_alpha}")
+
+    def show_spacecraft_all_info(self):
+        self.show_spacecraft_basic_info()
+        self.show_spacecraft_gnc_info()
+        self.show_spacecraft_state_info()
+        self.show_spacecraft_reference_info()    
+        
+    def get_gnc_dts(self):
+        return self.spacecraft_data.dt_nav, self.spacecraft_data.dt_guid, self.spacecraft_data.dt_control
+    
+    def __compute_navigation(self):
+        for sensor in self.spacecraft_data.sensors:
+
+            true_state = np.concatenate((
+                self.spacecraft_data.true_pos,
+                self.spacecraft_data.true_vel,
+                self.spacecraft_data.true_accel,
+                self.spacecraft_data.true_q,
+                self.spacecraft_data.true_omega,
+                self.spacecraft_data.true_alpha
+                ))
+            true_ref_state = np.concatenate((
+                self.spacecraft_data.true_ref_pos,
+                self.spacecraft_data.true_ref_vel,
+                self.spacecraft_data.true_ref_accel,
+                self.spacecraft_data.true_ref_q,
+                self.spacecraft_data.true_ref_omega,
+                self.spacecraft_data.true_ref_alpha
+                ))
+
+            sensor_output = sensor.get_output(true_state, true_ref_state)
+
+            if sensor.get_type() == "IMU":
+                pass
+            if sensor.get_type() == "GPS":
+                pass
+            
+            
+            # if sensor.get_type() == "Gyroscope":
+            #     pass
+            # if sensor.get_type() == "Accelerometer":
+            #     pass
+            # if sensor.get_type() == "Star Tracker":
+            #     pass
+            # if sensor.get_type() == "Magnetometer":
+            #     pass
+            # if sensor.get_type() == "Sun Sensor":
+            #     pass
+            # if sensor.get_type() == "LIDAR":
+            #     pass
+            # if sensor.get_type() == "Camera":
+            #     pass
+
+
+                
+
+
+        # Placeholder for navigation computation logic
+        pass
+
+    def __compute_guidance(self):
+        # Placeholder for guidance computation logic
+        pass
+
+    def __compute_control(self):
+        # Placeholder for control computation logic
+        pass
+
+    def __compute_actuation(self):
+        # Placeholder for actuation computation logic
+        pass
+
+    def compute_tick_step(self, tick: int, dt_master: float):        
+
+        # Check if it's time to compute navigation
+        if tick % int(self.spacecraft_data.dt_nav / dt_master) == 0:
+            self.__compute_navigation()
+
+        # Check if it's time to compute guidance
+        if tick % int(self.spacecraft_data.dt_guid / dt_master) == 0:
+            self.__compute_guidance()
+
+        # Check if it's time to compute control
+        if tick % int(self.spacecraft_data.dt_control / dt_master) == 0:
+            self.__compute_control()
+
